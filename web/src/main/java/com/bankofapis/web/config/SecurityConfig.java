24b70d9403b2c51570fbd43e7c836c1ab6fce104
vikram <singh.vikram456@gmail.com>
2020-07-14 16:39:11 +0530

    security config changes

--- web/src/main/java/com/bankofapis/web/config/SecurityConfig.java
+++ web/src/main/java/com/bankofapis/web/config/SecurityConfig.java
@@ -17,7 +17,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 //        http.csrf().disable();
-        http.cors();
+        http.authorizeRequests().anyRequest().permitAll();
+        http.cors().and().csrf().disable();;
     }
 
     @Bean
